ba99892c1fda4d87a70cd488a1ea5ae3727833b2
wasiim105 <wasimraza105@gmail.com>
2025-10-22 17:12:15 +0530

    remove eslint errors

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,22 @@ import globals from "globals";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
+
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
   pluginReact.configs.flat.recommended,
+  {
+    ignores: ['dist/**', 'build/**', 'node_modules/**', '*.config.js'],
+  },
+  {
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      'react/prop-types': 'off'
+    }
+  },
+  
 ]);
--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -1,17 +1,14 @@
 import React, {useEffect, useState} from 'react'
 import './PlayVideo.css'
-import video1 from '../../assets/video.mp4'
 import like from '../../assets/like.png'
 import dislike from '../../assets/dislike.png'
 import share from '../../assets/share.png'
 import save from '../../assets/save.png'
-import jack from '../../assets/jack.png'
-import user_profile from '../../assets/user_profile.jpg'
 import { API_KEY, value_converter } from '../../data'
 import moment from 'moment'
 import { useParams } from 'react-router-dom'
 
-const PlayVideo = ({}) => {
+const PlayVideo = () => {
 
   const {videoId} = useParams();
 
@@ -47,7 +44,7 @@ const PlayVideo = ({}) => {
   return (
     <div className='play-video'>
         {/*<video src={video1} controls autoPlay muted></video>*/}
-      <iframe src={`https://www.youtube.com/embed/${videoId}?autoplay=1`} frameborder="0" allow="accelerometer;
+      <iframe src={`https://www.youtube.com/embed/${videoId}?autoplay=1`} frameBorder="0" allow="accelerometer;
          autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         <h3>{apiData?apiData.snippet.title:"Title Here"}</h3>
         <div className="play-video-info">
